0f15824affdaf3571b3cd40b89aaaf7ce2cf56c0
Brandon Him <brh55@drexel.edu>
2017-04-14 16:55:33 -0700

    Add multi-column support

--- components/Masonry.js
+++ components/Masonry.js
@@ -1,5 +1,4 @@
 'use strict';
-
 import { ListView, Image } from 'react-native';
 import React, { Component } from 'react';
 import Row from './Row';
@@ -11,12 +10,12 @@ export default class Masonry extends Component {
   };
 
   static defaultProps = {
-    images: []
+      images: [],
+      columns: 2
   };
 
   constructor(props) {
     super(props);
-
     this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
     this.state = {
       dataSource: this.ds
@@ -29,18 +28,17 @@ export default class Masonry extends Component {
         };
       })
       .catch(console.log);
-
   }
 
   componentWillReceiveProps(nextProps) {
     Promise.all(__getImages(nextProps.data))
-      .then(fetchedImages => {
-        const images = __splitIntoColumns(fetchedImages);
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows([...images])
-        });
-      })
-      .catch(console.log);
+	  .then(fetchedImages => {
+      const images = __splitIntoColumns(fetchedImages, nextProps.columns);
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows([...images])
+      });
+    })
+	  .catch(console.log);
   }
 
   render() {
@@ -48,12 +46,11 @@ export default class Masonry extends Component {
        <ListView
         contentContainerStyle={ styles.masonry__container }
         dataSource={ this.state.dataSource }
-        renderRow={ (data) => <Row {...data} /> }
+        renderRow={ (data) => (<Row data={data} columns={this.props.columns} />) }
       />
     )
   }
 }
-
 
 export function __getImages(images) {
   return images.map((image) => pGetImageSize(image.uri).then((dimensions) => {
@@ -82,6 +79,6 @@ export function __splitIntoColumns(data, nColumns = 2) {
 
 function pGetImageSize(uri) {
   return new Promise((resolve, reject) => {
-      Image.getSize(uri, (width, height) => resolve([width, height]), (err) => reject(err));
+    Image.getSize(uri, (width, height) => resolve([width, height]), (err) => reject(err));
   });
 }
--- components/Row.js
+++ components/Row.js
@@ -3,10 +3,11 @@ import { Dimensions, View, Image } from 'react-native';
 import styles from '../styles/main';
 
 const Row = (props) => {
+  const data = props.data;
   // Resize based on columns
-  const images = Object.keys(props).map((key) => {
-    const { width, height, gutter } = __resizeByColumns(props[key].dimensions, 2);
-    return { uri: props[key].uri, width, height, gutter };
+  const images = Object.keys(data).map((key) => {
+	   const { width, height, gutter } = __resizeByColumns(data[key].dimensions, props.columns);
+	   return { uri: data[key].uri, width, height, gutter };
   });
 
   return (
@@ -16,7 +17,7 @@ const Row = (props) => {
   )
 }
 
-export function __resizeByColumns (imgDimensions, nColumns) {
+export function __resizeByColumns (imgDimensions, nColumns=2) {
     const { height, width } = Dimensions.get('window');
 
     const gutterBase = width / 100; // 1% = X px
@@ -32,10 +33,16 @@ export function __resizeByColumns (imgDimensions, nColumns) {
 }
 
 export function __renderImages (images) {
-  return images.map((image, index) => {
+    return images.map((image, index) => {
     // Avoid margins for first element
     const gutter = (index === 0) ? 0 : image.gutter;
-    return (<Image key={image.uri} source={{ uri: image.uri }} resizeMethod='auto' style={{ width: image.width, height: image.height, marginTop: gutter }} />);
+    return (
+    	<Image
+    	  key={image.uri}
+    	  source={{ uri: image.uri }}
+    	  resizeMethod='auto'
+    	  style={{ width: image.width, height: image.height, marginTop: gutter }} />
+    );
   });
 }
 
