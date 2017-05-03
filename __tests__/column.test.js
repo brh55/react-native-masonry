bda4f6ab960ae21568c89a6f18b06a2a53e642a8
Brandon Him <brh55@drexel.edu>
2017-05-03 00:40:34 -0700

    Add additional column test

--- __tests__/column.test.js
+++ __tests__/column.test.js
@@ -41,7 +41,7 @@ test('PRIVATE FUNC: Render bricks properly', () => {
   expect(JSON.stringify(bricks[0])).toEqual(JSON.stringify(_getTouchableUnit(mock[0], 0)));
 });
 
-test('PRIVATE FUNC: Renders image tag', () => {
+test('PRIVATE FUNC: Renders image tag', () => 
   const imageTag = _getImageTag(mock[0], 9);
   expect(imageTag).toEqual(
       <Image key='http://test1.jpg' source={{ uri: 'http://test1.jpg' }} resizeMethod="auto" style={{ width: 100, height: 200, marginTop: 9 }} />);
