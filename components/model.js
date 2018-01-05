4d4395ef8230dd282de37cda339b05798efa2e2f
Brandon Him <brandonhim@live.com>
2018-01-05 11:49:15 -0600

    Fix indentations and bump

--- components/model.js
+++ components/model.js
@@ -3,12 +3,12 @@ import Task from 'data.task';
 
 // resolveImage :: String -> Task(Error, Image)
 export const resolveImage = (data) => {
-  return new Task((reject, resolve) => Image.getSize(data.uri, (width, height) => resolve({
-    ...data,
-    //@TODO: Consider consolidating data.uri with dimensions to a image object
-    dimensions: {
-      width,
-      height
-    }
-  }), (err) => reject(err)));
+	return new Task((reject, resolve) => Image.getSize(data.uri, (width, height) => resolve({
+		...data,
+		//@TODO: Consider consolidating data.uri with dimensions to a image object
+		dimensions: {
+			width,
+			height
+		}
+	}), (err) => reject(err)));
 };
